87ac88448ea283386e916037c0b09e8c2298cadd
MfalconP <maryushp@gmail.com>
2023-05-23 19:18:15 +0200

    fix(budget_calculator) old pages deleted, new pages added

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QWidget
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
+import ui.background_rc
 
 
-# Start window class
-class StartWindow(QMainWindow):
+# Login window class
+class LoginPage(QWidget):
     def __init__(self):
-        super(StartWindow, self).__init__()
-        loadUi("ui/StartWindow.ui", self)
+        super(LoginPage, self).__init__()
+        loadUi("ui/LoginPage.ui", self)
 
         self.signInButton.clicked.connect(self.loginFunction)
         self.createAccButton.clicked.connect(self.gotoSignUpWindow)
-        self.passwordEnterText.setEchoMode(QtWidgets.QLineEdit.Password)
 
-    # login function, needs to be connected with logic and database
+        # login function, needs to be connected with logic and database
     def loginFunction(self):
         login = self.loginEnterText.text()
         password = self.passwordEnterText.text()
         print(f"Logged with login: {login} and password: {password}")  # this print may be deleted in future
 
     def gotoSignUpWindow(self):
-        createAccWindow = CreateAccWindow()
+        createAccWindow = SignUpPage()
         widget.addWidget(createAccWindow)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
 # Sign up window class
-class CreateAccWindow(QMainWindow):
+class SignUpPage(QWidget):
     def __init__(self):
-        super(CreateAccWindow, self).__init__()
-        loadUi("ui/CreateAccWindow.ui", self)
+        super(SignUpPage, self).__init__()
+        loadUi("ui/SignUpPage.ui", self)
         self.signUpButton.clicked.connect(self.signUpFunction)
-        self.passwordText.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.confirmPasText.setEchoMode(QtWidgets.QLineEdit.Password)
 
     # Sign Up button function -needs to be updated and connected with logic and database
     def signUpFunction(self):
@@ -42,20 +40,19 @@ class CreateAccWindow(QMainWindow):
             login = self.loginText.text()
             password = self.passwordText.text()
             print(f"Sign up successfully, login: {login}, password: {password}")  # this print may be deleted in future
-
             # returning to the start window
-            startWindow = StartWindow()
-            widget.addWidget(startWindow)
+            loginWindow = LoginPage()
+            widget.addWidget(loginWindow)
             widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = StartWindow()
+    startWindow = LoginPage()
     widget = QtWidgets.QStackedWidget()
-    widget.addWidget(mainWindow)
-    widget.setFixedSize(1143, 736)
+    widget.addWidget(startWindow)
+    widget.setFixedSize(549, 626)
     widget.show()
     app.exec_()
 
