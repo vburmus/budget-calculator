9ac37893481a47e134bdb5a8e1e2e6ed68a93c41
vburmus <269545@student.pwr.edu.pl>
2023-05-25 15:03:09 +0200

    feat(budget_calculator) added some methods to account repository

--- logic/repositories.py
+++ logic/repositories.py
@@ -47,6 +47,38 @@ class AccountRepository:
         self.connection = DataSource.get_connection()
         self.cursor = self.connection.cursor()
 
+    def create_new_account(self, account):
+        return self.cursor.execute(CREATE_ACCOUNT_QUERY,
+                                   (account.name, account.description, account.password, account.balance))
+
+    def get_accounts_by_user(self, user):
+        self.cursor.execute(GET_ACCOUNTS_BY_USER_QUERY, (user.id,))
+        result = self.cursor.fetchall()
+        accounts = []
+
+        for account in result:
+            accounts.append(self.parse_account(account))
+        return accounts
+
+    def get_account_by_id(self, id):
+        self.cursor.execute(GET_ACCOUNT_BY_ID_QUERY, (id,))
+        result = self.cursor.fetchone()
+        account = self.parse_account(result)
+        return account
+
+    def update_account(self, account):
+        self.cursor.execute(UPDATE_ACCOUNT_QUERY, (
+            account.name, account.description, account.user.id, account.balance,account.id,));
+
+    @staticmethod
+    def parse_account(account):
+        if account is None:
+            return None
+        user_repository = UserRepository()
+        user = user_repository.get_user_by_id(account[4])
+        return Account(id=account[0], name=account[1], balance=account[2],
+                       description=account[3], user=user)
+
 
 class TransactionRepository:
     def __init__(self):
