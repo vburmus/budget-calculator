376a1e626c857328dbad72eff85f4df6b0e0a82f
vburmus <269545@student.pwr.edu.pl>
2023-05-25 15:02:51 +0200

    feat(budget_calculator) added all methods to user repository

--- logic/repositories.py
+++ logic/repositories.py
@@ -20,8 +20,25 @@ class UserRepository:
         logger.info(result)
         return user
 
+    def get_user_by_id(self, id):
+        self.cursor.execute(GET_USER_BY_ID_QUERY, (id,))
+        result = self.cursor.fetchone()
+
+        user = self.parse_user(result)
+        logger.info(result)
+        return user
+
     def get_current_user_balance(self, user):
-        return self.cursor.execute("SELECT balance FROM user WHERE login = ?", (user.login,))
+        return self.cursor.execute(GET_CURRENT_USER_BALANCE_QUERY, (user.login,))
+
+    def delete_user(self, user):
+        return self.cursor.execute(DELETE_USER_QUERY, (user.login,))
+
+    @staticmethod
+    def parse_user(user):
+        if user is None:
+            return None
+        return User(id=user[0], login=user[1], password=user[2], balance=float(user[3]))
 
 
 class AccountRepository:
