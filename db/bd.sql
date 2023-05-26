08ec232b3bacdbc0d6292e9cb290e84b11db62d6
vburmus <269545@student.pwr.edu.pl>
2023-05-26 12:15:58 +0200

    fix(budget_calculator) modified bd(deleted type,added cascades, refactored category)

--- db/bd.sql
+++ db/bd.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8mb3 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
@@ -21,11 +21,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`user` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `login` VARCHAR(45) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `balance` FLOAT 0,
+  `balance` FLOAT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -34,29 +35,19 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `mydb`.`account` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
   `user_id` BIGINT NOT NULL,
-  `balance` FLOAT 0,
-   PRIMARY KEY (`id`),
+  `balance` FLOAT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   INDEX `fk_Account_User1_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_Account_User1`
     FOREIGN KEY (`user_id`)
     REFERENCES `mydb`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`type`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`type` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -65,9 +56,18 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `mydb`.`category` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
+  `user_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `fk_category_user1_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_category_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -76,61 +76,28 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `mydb`.`transaction` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `amount` FLOAT NOT NULL,
-  `description` VARCHAR(45) NULL,
-  `date` DATETIME NOT NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
+  `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `account_id` BIGINT NOT NULL,
-  `type_id` BIGINT NOT NULL,
   `category_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   INDEX `fk_Spend_Account1_idx` (`account_id` ASC) VISIBLE,
-  INDEX `fk_Spend_Type1_idx` (`type_id` ASC) VISIBLE,
   INDEX `fk_Spend_Category1_idx` (`category_id` ASC) VISIBLE,
   CONSTRAINT `fk_Spend_Account1`
     FOREIGN KEY (`account_id`)
     REFERENCES `mydb`.`account` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Spend_Type1`
-    FOREIGN KEY (`type_id`)
-    REFERENCES `mydb`.`type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_Spend_Category1`
     FOREIGN KEY (`category_id`)
     REFERENCES `mydb`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `mydb`.`type`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `mydb`;
-INSERT INTO `mydb`.`type` (`id`, `name`) VALUES (1, 'income');
-INSERT INTO `mydb`.`type` (`id`, `name`) VALUES (2, 'expense');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `mydb`.`category`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `mydb`;
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (1, 'Food');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (2, 'Housing');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (3, 'Transport');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (4, 'Vehicle');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (5, 'Entertainment');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (6, 'Shopping');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (7, 'Communication');
-INSERT INTO `mydb`.`category` (`id`, `name`) VALUES (8, 'Default');
-COMMIT;
-
