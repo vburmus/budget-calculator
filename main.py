581a945a30b725f9a2003383c0a8bb0a513d8502
MfalconP <maryushp@gmail.com>
2023-05-26 21:27:29 +0200

    fix(budget_calculator) refactored code

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
+
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import QtWidgets
-from PyQt5.uic import loadUi
+from PyQt5 import uic
+import ui.background_rc
 
 from logic.services import *
 from loguru import logger
-import ui.background_rc
 
 
 # Login window class
 class LoginPage(QWidget):
     def __init__(self):
         super(LoginPage, self).__init__()
-        loadUi("ui/LoginPage.ui", self)
+        uic.loadUi("ui/LoginPage.ui", self)
 
         self.signInButton.clicked.connect(self.login_function)
         self.createAccButton.clicked.connect(self.goto_sign_up)
@@ -21,9 +22,7 @@ class LoginPage(QWidget):
         self.user_service = UserService()
 
     def login_function(self):
-        login = self.loginEnterText.text()
-        password = self.passwordEnterText.text()
-        success, message = self.user_service.login(login, password)
+        success, message = self.user_service.login(self.loginEnterText.text(), self.passwordEnterText.text())
 
         if not success:
             self.communicateTextLabel.setText(message)
@@ -46,18 +45,17 @@ class LoginPage(QWidget):
 class SignUpPage(QWidget):
     def __init__(self):
         super(SignUpPage, self).__init__()
-        loadUi("ui/SignUpPage.ui", self)
+        uic.loadUi("ui/SignUpPage.ui", self)
+
         self.signUpButton.clicked.connect(self.sign_up_function)
-        self.exitButton.clicked.connect(self.exit)
+        self.exitButton.clicked.connect(self.return_to_login_page)
+
         self.user_service = UserService()
 
     def sign_up_function(self):
-        login = self.loginText.text()
-        password = self.passwordText.text()
-        confirm_password = self.confirmPasText.text()
-
-        success, message = self.user_service.register(login, password, confirm_password)
-
+        success, message = self.user_service.register(self.loginText.text(),
+                                                      self.passwordText.text(),
+                                                      self.confirmPasText.text())
         if not success:
             self.communicateTextLabel.setText(message)
             logger.warning(message)
@@ -69,7 +67,7 @@ class SignUpPage(QWidget):
             widget.addWidget(loginWindow)
             widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def exit(self):
+    def return_to_login_page(self):
         loginWindow = LoginPage()
         widget.addWidget(loginWindow)
         widget.setCurrentIndex(widget.currentIndex() + 1)
@@ -78,14 +76,18 @@ class SignUpPage(QWidget):
 class MainPage(QWidget):
     def __init__(self, user):
         super(MainPage, self).__init__()
-        loadUi("ui/MainPage.ui", self)
+        uic.loadUi("ui/MainPage.ui", self)
+
         self.signOutButton.clicked.connect(self.sign_out_function)
-        self.user = user
         self.settingsButton.clicked.connect(self.user_settings)
+        self.addAccountButton.clicked.connect(self.goto_adding_new_account)
+
         self.userName.setText(user.login)
+
+        self.user = user
+
         self.accountDescription.setText("")
         self.transactionDetails.setText("")
-        self.addAccountButton.clicked.connect(self.goto_adding_new_account)
 
     def sign_out_function(self):
         loginWindow = LoginPage()
@@ -110,14 +112,17 @@ class MainPage(QWidget):
 class UserSettingsPage(QWidget):
     def __init__(self, user):
         super(UserSettingsPage, self).__init__()
-        loadUi("ui/UserSettingsPage.ui", self)
-        self.user = user
-        self.refresh_labels(user)
+        uic.loadUi("ui/UserSettingsPage.ui", self)
+
         self.exitButton.clicked.connect(self.exit)
         self.submitButton.clicked.connect(self.submit_changes)
-        self.user_service = UserService()
-        self.communicateTextLabel.setText("")
         self.deleteAccountButton.clicked.connect(self.delete_account)
+
+        self.communicateTextLabel.setText("")
+        self.refresh_username_labels(user)
+
+        self.user_service = UserService()
+        self.user = user
 
     def delete_account(self):
         success, message = self.user_service.delete(self.user, self.passwordText.text())
@@ -130,7 +135,7 @@ class UserSettingsPage(QWidget):
             self.communicateTextLabel.setText(message)
             self.passwordText.setText("")
 
-    def refresh_labels(self, user):
+    def refresh_username_labels(self, user):
         self.userName.setText(user.login)
         self.userNameTextEdit.setPlaceholderText(user.login)
 
@@ -141,27 +146,31 @@ class UserSettingsPage(QWidget):
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def submit_changes(self):
-        success, message = self.user_service.update(self.user, self.passwordText.text(),
-                                                    self.userNameTextEdit.text(), self.newPasswordText.text())
+        success, response = self.user_service.update(self.user, self.passwordText.text(),
+                                                     self.userNameTextEdit.text(), self.newPasswordText.text())
+
         self.communicateTextLabel.setText("")
         self.userNameTextEdit.setText("")
         self.passwordText.setText("")
         self.newPasswordText.setText("")
+
         if success:
-            self.user = message
-            self.refresh_labels(self.user)
+            self.user = response
+            self.refresh_username_labels(self.user)
         else:
-            self.communicateTextLabel.setText(message)
+            self.communicateTextLabel.setText(response)
 
 
 class AddAccountPage(QWidget):
     def __init__(self, user):
         super(AddAccountPage, self).__init__()
-        loadUi("ui/AddAccountPage.ui", self)
-        self.user = user
+        uic.loadUi("ui/AddAccountPage.ui", self)
+
         self.exitButton.clicked.connect(self.exit)
         self.addButton.clicked.connect(self.add_new_account)
+
         self.acc_service = AccountService()
+        self.user = user
 
     def exit(self):
         mainWindow = MainPage(self.user)
